774f12ca3b34caa748de297804b76e5ffa9705be
TrainingSSS <80456853+TrainingSSS@users.noreply.github.com>
2021-03-11 02:24:26 -0800

    Add files via upload

---
+++ Login.py
@@ -0,0 +1,12 @@
+User_name = input('Enter your User name:')
+Password = input('Enter your Password:')
+atanabain = {
+    'Username' : 'atanabain',
+    'Password' : 'a87948794',
+    'Aga' : 13,
+    'Languge Programing' : 'Python',
+    'OS' : 'Windows',
+    }
+if User_name == 'atanabain':
+    for i in atanabain.keys() , atanabain.values() :
+        print(i)
